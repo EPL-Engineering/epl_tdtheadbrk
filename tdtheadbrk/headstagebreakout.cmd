537acc767270542116b22d0a124bd5790bd0db5b
Evan Foss <evanfoss@gmail.com>
2012-08-06 16:52:15 -0500

    Initial Commit

---
+++ tdtheadbrk/headstagebreakout.cmd
@@ -0,0 +1,107 @@
+# Pin name action command file
+
+# Start of element 26_NA
+ChangePinName(26_NA, 1, 1)
+
+# Start of element 25_A16
+ChangePinName(25_A16, 1, 1)
+
+# Start of element 24_A14
+ChangePinName(24_A14, 1, 1)
+
+# Start of element 23_A12
+ChangePinName(23_A12, 1, 1)
+
+# Start of element 22_A10
+ChangePinName(22_A10, 1, 1)
+
+# Start of element 21_A8
+ChangePinName(21_A8, 1, 1)
+
+# Start of element 20_A6
+ChangePinName(20_A6, 1, 1)
+
+# Start of element 19_HSD
+ChangePinName(19_HSD, 1, 1)
+
+# Start of element 18_HSD
+ChangePinName(18_HSD, 1, 1)
+
+# Start of element 17_V-
+ChangePinName(17_V-, 1, 1)
+
+# Start of element 16_GND
+ChangePinName(16_GND, 1, 1)
+
+# Start of element 15_GND
+ChangePinName(15_GND, 1, 1)
+
+# Start of element 14_V+
+ChangePinName(14_V+, 1, 1)
+
+# Start of element 13_GND
+ChangePinName(13_GND, 1, 1)
+
+# Start of element 12_A15
+ChangePinName(12_A15, 1, 1)
+
+# Start of element 11_A13
+ChangePinName(11_A13, 1, 1)
+
+# Start of element 10_A11
+ChangePinName(10_A11, 1, 1)
+
+# Start of element 9_A9
+ChangePinName(9_A9, 1, 1)
+
+# Start of element 8_A7
+ChangePinName(8_A7, 1, 1)
+
+# Start of element 7_A5
+ChangePinName(7_A5, 1, 1)
+
+# Start of element 6_HSD
+ChangePinName(6_HSD, 1, 1)
+
+# Start of element 5_Ref
+ChangePinName(5_Ref, 1, 1)
+
+# Start of element 4_A4
+ChangePinName(4_A4, 1, 1)
+
+# Start of element 3_A3
+ChangePinName(3_A3, 1, 1)
+
+# Start of element 2_A2
+ChangePinName(2_A2, 1, 1)
+
+# Start of element 1_A1
+ChangePinName(1_A1, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 25, 25)
+ChangePinName(CONN1, 26, 26)
+ChangePinName(CONN1, 23, 23)
+ChangePinName(CONN1, 21, 21)
+ChangePinName(CONN1, 19, 19)
+ChangePinName(CONN1, 17, 17)
+ChangePinName(CONN1, 24, 24)
+ChangePinName(CONN1, 22, 22)
+ChangePinName(CONN1, 20, 20)
+ChangePinName(CONN1, 18, 18)
+ChangePinName(CONN1, 15, 15)
+ChangePinName(CONN1, 13, 13)
+ChangePinName(CONN1, 11, 11)
+ChangePinName(CONN1, 9, 9)
+ChangePinName(CONN1, 16, 16)
+ChangePinName(CONN1, 14, 14)
+ChangePinName(CONN1, 12, 12)
+ChangePinName(CONN1, 10, 10)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
